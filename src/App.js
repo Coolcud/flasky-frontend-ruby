c03af4a7156bacab2051df562d86672430483310
Sage Choi <coolcuds@gmail.com>
2023-06-15 10:13:58 -0700

    Added functionality to display different restaurants from the restaurant list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import RestaurantList from './components/RestaurantList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const data = [
+        {id: 1, name: "Salty's", cuisine: "Seafood", rating: 4, distance: "5 miles"},
+        {id: 2, name: "Toulouse", cuisine: "Creole", rating: 2, distance: "2 miles"},
+        {id: 3, name: "Tanoor", cuisine: "Arab", rating: 4, distance: "2.5 miles"},
+        {id: 4, name: "Meet", cuisine: "Korean BBQ", rating: 5, distance: "1 mile"}
+    ];
+
+    return (
+        <div className='App'>
+            <RestaurantList data={data}/>
+        </div>
+    );
 }
 
 export default App;
