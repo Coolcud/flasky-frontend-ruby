9c013f4ecbea380dc85c74b21ccff5d954ac6b7b
Sage Choi <coolcuds@gmail.com>
2023-06-15 11:03:37 -0700

    Added react hook to update state of rating

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,13 +1,37 @@
-import "./Restaurant.css";
+import './Restaurant.css';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const Restaurant = ({ id, name, cuisine, rating, distance }) => {
+  const [updateRating, setRating] = useState(rating);
+
+  const changeRating = (direction) => {
+    if (direction === 'up') {
+      setRating(updateRating + 1);
+    } else if (direction === 'down') {
+      setRating(updateRating - 1);
+    }
+  };
+
+  const determineColor = () => {
+    if (updateRating > 5) {
+      return 'high-rating';
+    } else if (updateRating > 3) {
+      return 'rating';
+    } else {
+      return 'low-rating';
+    }
+  }
+
   return (
     <div>
-      <h1 className="restaurant-title">{name}</h1>
+      <h2 className="restaurant-title">{name}</h2>
       <ul>
+        <li>ID: {id}</li>
         <li>Cuisine: {cuisine}</li>
-        <li>Rating: {rating}</li>
+        <li className={determineColor()}>Rating: {updateRating}</li>
+        <button onClick={() => changeRating('up')}>Upvote</button>
+        <button onClick={() => changeRating('down')}>Downvote</button>
         <li>Distance from Ada: {distance}</li>
       </ul>
     </div>
