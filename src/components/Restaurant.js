a5690650dd33f814cc737f6b851f87760ad1ad4f
Sage Choi <coolcuds@gmail.com>
2023-06-16 12:12:31 -0700

    Move state change functionality to App for SSOT

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,22 +1,12 @@
 import './Restaurant.css';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-const Restaurant = ({ id, name, cuisine, rating, distance }) => {
-  const [updateRating, setRating] = useState(rating);
-
-  const changeRating = (direction) => {
-    if (direction === 'up') {
-      setRating(updateRating + 1);
-    } else if (direction === 'down') {
-      setRating(updateRating - 1);
-    }
-  };
-
+const Restaurant = ({ id, name, cuisine, rating, distance, updateRating, deleteRestaurant}) => {
   const determineColor = () => {
-    if (updateRating > 5) {
+    if (rating > 5) {
       return 'high-rating';
-    } else if (updateRating > 3) {
+    } else if (rating > 3) {
       return 'rating';
     } else {
       return 'low-rating';
@@ -27,11 +17,11 @@ const Restaurant = ({ id, name, cuisine, rating, distance }) => {
     <div>
       <h2 className="restaurant-title">{name}</h2>
       <ul>
-        <li>ID: {id}</li>
         <li>Cuisine: {cuisine}</li>
-        <li className={determineColor()}>Rating: {updateRating}</li>
-        <button onClick={() => changeRating('up')}>Upvote</button>
-        <button onClick={() => changeRating('down')}>Downvote</button>
+        <li className={determineColor()}>Rating: {rating}</li>
+        <button onClick={() => updateRating(id, rating, 'up')}>Upvote</button>
+        <button onClick={() => updateRating(id, rating, 'down')}>Downvote</button>
+        <button onClick={() => deleteRestaurant(id)}>Delete</button>
         <li>Distance from Ada: {distance}</li>
       </ul>
     </div>
