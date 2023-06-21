e8ac14da8a8c43513362ae7248814a5d7081e5cf
Sage Choi <coolcuds@gmail.com>
2023-06-21 11:55:59 -0700

    Integrate API and axios to modify database

--- src/App.js
+++ src/App.js
@@ -1,52 +1,75 @@
 import './App.css';
 import RestaurantList from './components/RestaurantList';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const restaurantData = [
-  {id: 1, name: "Salty's", cuisine: "Seafood", rating: 4, distance: "5 miles"},
-  {id: 2, name: "Toulouse", cuisine: "Creole", rating: 2, distance: "2 miles"},
-  {id: 3, name: "Tanoor", cuisine: "Arab", rating: 4, distance: "2.5 miles"},
-  {id: 4, name: "Meet", cuisine: "Korean BBQ", rating: 5, distance: "1 mile"}
-];
 
 function App() {
-  const [restaurants, setRestaurants] = useState(restaurantData);
+  const [restaurants, setRestaurants] = useState([]);
+  const API = "https://nancy-harris-ruby-restaurant-flasky.onrender.com/restaurant";
 
+  useEffect(() => {
+    axios.get(API)
+      .then((result) => {
+        setRestaurants(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  // changeRating that updates state manually
   const changeRating = (id, originalRating, direction) => {
-    const newRestaurants = restaurants.map((restaurant) => {
-      if (restaurant.id === id) {
-        const updatedRestaurant = {...restaurant};
-        if (direction === 'up') {
-          updatedRestaurant.rating = originalRating + 1;
-        } else if (direction === 'down') {
-          updatedRestaurant.rating = originalRating - 1;
-        }
-        return updatedRestaurant;
-      } else {
-        return {...restaurant};
-      }
-    });
+    const newRating = direction === "up" ? originalRating + 1 : originalRating - 1;
 
-    setRestaurants(newRestaurants);
+    axios.patch(`${API}/${id}/rating`, { value: newRating })
+      .then((result) => {
+        console.log(result.data);
+        const newRestaurants = restaurants.map((restaurant) => {
+          if (restaurant.id === id) {
+            const updatedRestaurant = { ...restaurant, rating: newRating };
+            return updatedRestaurant;
+          }
+          return { ...restaurant };
+        });
+        setRestaurants(newRestaurants);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
+  // changeRating that updates state via API call
+  // const changeRating = (id, originalRating, direction) => {
+  //   const newRating = direction === "up" ? originalRating + 1 : originalRating - 1;
+
+  //   axios.patch(`${API}/${id}/rating`, { value: newRating })
+  //     .then((result) => {
+  //       console.log(result.data);
+  //       axios.get(API)
+  //         .then((result) => {
+  //           setRestaurants(result.data);
+  //         })
+  //         .catch((err) => {
+  //           console.log(err);
+  //         });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
   const deleteRestaurant = (id) => {
-    setRestaurants(restaurants => {
-      return restaurants.filter(rest => rest.id !== id);
-    });
-
-    // const newRestaurants = restaurants.filter(
-    //   (restaurant) => restaurant.id !== id
-    // );
-    // setRestaurants(newRestaurants);
-
-    // const newRestaurants = [];
-    // for (let restaurant of restaurants) {
-    //   if (restaurant.id !== id) {
-    //     newRestaurants.push(restaurant);
-    //   }
-    // }
-    // setRestaurants(newRestaurants);
+    axios.delete(`${API}/${id}`)
+      .then((result) => {
+        console.log(result.data);
+        setRestaurants(restaurants => {
+          return restaurants.filter(rest => rest.id !== id);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -55,9 +78,40 @@ function App() {
         restaurantData={restaurants}
         updateRating={changeRating}
         deleteRestaurant={deleteRestaurant}
-      />
+        />
     </div>
   );
 }
 
 export default App;
+
+
+/*
+RESTAURANT DATA BEFORE AXIOS
+
+const restaurantData = [
+  {id: 1, name: "Salty's", cuisine: "Seafood", rating: 4, distance: "5 miles"},
+  {id: 2, name: "Toulouse", cuisine: "Creole", rating: 2, distance: "2 miles"},
+  {id: 3, name: "Tanoor", cuisine: "Arab", rating: 4, distance: "2.5 miles"},
+  {id: 4, name: "Meet", cuisine: "Korean BBQ", rating: 5, distance: "1 mile"}
+];
+
+OTHER WAYS TO DELETE RESTAURANT
+
+    setRestaurants(restaurants => {
+      return restaurants.filter(rest => rest.id !== id);
+    });
+
+    const newRestaurants = restaurants.filter(
+      (restaurant) => restaurant.id !== id
+    );
+    setRestaurants(newRestaurants);
+
+    const newRestaurants = [];
+    for (let restaurant of restaurants) {
+      if (restaurant.id !== id) {
+        newRestaurants.push(restaurant);
+      }
+    }
+    setRestaurants(newRestaurants);
+*/
